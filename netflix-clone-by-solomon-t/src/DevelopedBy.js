e3c7a8dcc3ee76fb64b66e27fd2f7daaf83a861e
Solomon Tagesse <solomontagesse478@gmail.com>
2024-05-22 17:07:44 +0300

    final code upload

---
+++ netflix-clone-by-solomon-t/src/DevelopedBy.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./DevelopedBy.css";
+const DevelopedBy = () => {
+  return (
+    <>
+      <div className="develop-wrapper">
+        <h1>Developed By Solomon Tagesse</h1>
+      </div>
+    </>
+  );
+};
+
+export default DevelopedBy;
